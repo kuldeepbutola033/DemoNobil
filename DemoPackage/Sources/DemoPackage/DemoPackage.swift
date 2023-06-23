9a198c4d79aa58a7c151cc343a3c1c9fe9c3f9c4
Shivam <shivam@MacOSs-MacBook-Pro.local>
2023-06-23 11:13:45 +0530

    @changes for framework

---
+++ DemoPackage/Sources/DemoPackage/DemoPackage.swift
@@ -0,0 +1,6 @@
+public struct DemoPackage {
+    public private(set) var text = "Hello, World!sss"
+
+    public init() {
+    }
+}
---
+++ DemoPackage/Sources/MyCustomFile.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shivam on 22/06/23.
+//
+
+import Foundation
+
+public class MyCustomFile{
+    
+    public static func newFunction(){
+        print("Hello Shivamvvvv")
+    }
+}
+
